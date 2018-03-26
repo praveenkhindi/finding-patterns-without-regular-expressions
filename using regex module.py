7ca107f763de8a9e682b4c8daf73a44d89660f6f
Praveen K <Praveen007cool@gmail.com>
2018-03-26 23:15:07 +0530

    Update using regex module.py

--- using regex module.py
+++ using regex module.py
@@ -1,5 +1,4 @@
 import re
 mynumregex = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)')
-mo = mynumregex.search('my number is 886-175-0782.')
+mo = mynumregex.search(' number is 888-888-8888.')
 print( mo.group(3))
-                        
